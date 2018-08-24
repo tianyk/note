63b3f77363c850458ea214af8133281c9f4b86b2
tyk <yongketian@gmail.com>
2018-08-25 01:13:14 +0800

    include_code 支持引用网络代码片段

---
+++ scripts/include_code.js
@@ -0,0 +1,51 @@
+const includeCode = require('hexo/lib/plugins/tag/include_code')(hexo);
+const download = require('download');
+const mime = require('mime-types');
+const path = require('path');
+const SEP = path.sep;
+
+const langREG = /\/([a-zA-Z]+)$/;
+
+function getLang(filename) {
+    const mimeType = mime.lookup(filename);
+    if (!mimeType) return;
+
+    const match = mimeType.match(langREG);
+    if (match) return `lang:${match[1]}`;
+}
+
+async function includeCodeTag (args) {
+    if (args.length >= 1 && args.length <= 3 && args[args.length - 1].startsWith('http')) {
+        const { source_dir: sourceDir, code_dir: codeDir } = hexo.config;
+        let filename, lang, filepath;
+
+        if (args.length === 1) {
+            filepath = args[0];
+            filename = path.basename(filepath);
+            lang = getLang(filename);
+        } else if (args.length === 2) {
+            if (args[0].startsWith('lang')) {
+                lang = args[0];
+                filepath = args[1];
+                filename = path.basename(filepath);
+            } else {
+                filename = args[0];
+                lang = getLang(filename);
+                filepath = args[1];
+            }
+        } else if (args.length === 3) {
+            filename = args[0];
+            lang = args[1];
+            filepath = args[2];
+        }
+
+        await download(filepath, path.join(sourceDir, codeDir, 'autogeneration'), { filename });
+        args = [filename, lang, `autogeneration${SEP}${filename}`].filter(arg => !!arg);
+    }
+    console.log(args)
+    return includeCode(args);
+}
+
+
+hexo.extend.tag.register('include_code', includeCodeTag, { async: true });
+hexo.extend.tag.register('include-code', includeCodeTag, { async: true });
