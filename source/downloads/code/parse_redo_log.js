1aa88a6e879a91ba702269aa79ecb7bc1b470c46
tyk <yongketian@gmail.com>
2018-08-28 11:26:00 +0800

    解析redo log

--- source/downloads/code/parse_redo_log.js
+++ source/downloads/code/parse_redo_log.js
@@ -1,98 +1,102 @@
-function readInt64BE(buf, offset = 0) {
+function readUInt64BE(buf, offset = 0) {
     return parseInt(buf.slice(offset, offset + 8).toString('hex'), 16);
 }
 
-function _parseLogFileHeader(header) {
+function parseLogFileHeader(header) {
     let _readerIndex = 0;
-    const groupId = header.readInt32BE();
+    const groupId = header.readUInt32BE();
     _readerIndex += 4;
-    const startLSN = readInt64BE(header, _readerIndex);
+    const startLSN = readUInt64BE(header, _readerIndex);
 
     return { groupId, startLSN };
 }
 
-function _parseCheckpoint(checkpoint) {
+function parseCheckpoint(checkpoint) {
     let _readerIndex = 0;
-    const no = readInt64BE(checkpoint, _readerIndex);
+    const no = readUInt64BE(checkpoint, _readerIndex);
     _readerIndex += 8;
 
-    const lsn = readInt64BE(checkpoint, _readerIndex);
+    const lsn = readUInt64BE(checkpoint, _readerIndex);
     _readerIndex += 8;
 
-    const offset = checkpoint.readInt32BE(_readerIndex);
+    const offset = checkpoint.readUInt32BE(_readerIndex);
     _readerIndex += 4;
 
-    const bufferSize = checkpoint.readInt32BE(_readerIndex);
+    const bufferSize = checkpoint.readUInt32BE(_readerIndex);
     _readerIndex += 4;
 
-    const archivedLSN = readInt64BE(checkpoint, _readerIndex);
+    const archivedLSN = readUInt64BE(checkpoint, _readerIndex);
     _readerIndex += 8;
 
     _readerIndex += 256; // skip LOG_CHECKPOINT_ARRAY 
 
-    const checksum1 = checkpoint.readInt32BE(_readerIndex);
+    const checksum1 = checkpoint.readUInt32BE(_readerIndex);
     _readerIndex += 4;
 
-    const checksum2 = checkpoint.readInt32BE(_readerIndex);
+    const checksum2 = checkpoint.readUInt32BE(_readerIndex);
     _readerIndex += 4;
-
 
     return { no, lsn, offset, bufferSize, archivedLSN, checksum1, checksum2 };
 }
 
 function parseRedoLogHeader(header) {
     let _readerIndex = 0;
-    const logFileHeader = header.slice(_readerIndex, 512);
+    let logFileHeader = header.slice(_readerIndex, 512);
     _readerIndex += 512;
-    _parseLogFileHeader(logFileHeader);
+    logFileHeader = parseLogFileHeader(logFileHeader);
 
-    const checkpoint1 = header.slice(_readerIndex, _readerIndex + 512);
+    let checkpoint1 = header.slice(_readerIndex, _readerIndex + 512);
     _readerIndex += 512;
-    _parseCheckpoint(checkpoint1);
+    checkpoint1 = parseCheckpoint(checkpoint1);
 
     _readerIndex += 512; // skip 
-    const checkpoint2 = header.slice(_readerIndex, _readerIndex + 512);
-    _parseCheckpoint(checkpoint2);
+
+    let checkpoint2 = header.slice(_readerIndex, _readerIndex + 512);
+    _readerIndex += 512;
+    checkpoint2 = parseCheckpoint(checkpoint2);
+
+    return { logFileHeader, checkpoint1, checkpoint2 };
 }
 
 function parseRedoLogBlock(block) {
     let _readerIndex = 0;
 
-    let HdrNo = block.readInt32BE(_readerIndex);
+    let HdrNo = block.readUInt32BE(_readerIndex);
     HdrNo = HdrNo & 0x7fffffff;
     let flushBit = HdrNo & 0x80000000;
     _readerIndex += 4;
-    const hdrDataLen = block.readInt16BE(_readerIndex);
+    const hdrDataLen = block.readUInt16BE(_readerIndex);
     _readerIndex += 2;
-    const firstRecGroup = block.readInt16BE(_readerIndex);
+    const firstRecGroup = block.readUInt16BE(_readerIndex);
     _readerIndex += 2;
-    const checkpointNo = block.readInt32BE(_readerIndex);
+    const checkpointNo = block.readUInt32BE(_readerIndex);
     _readerIndex += 4;
 
     // console.log(block.slice(_readerIndex, _readerIndex + 496).toString('hex'))
     _readerIndex += 496; // skip data
 
-    const checksum = block.readInt32BE(_readerIndex);
+    const checksum = block.readUInt32BE(_readerIndex);
 
-    console.log(HdrNo, flushBit, hdrDataLen, firstRecGroup, checkpointNo, checksum);
+    return { HdrNo, flushBit, hdrDataLen, firstRecGroup, checkpointNo, checksum };
 }
 
 
 function parseRedoLog(redoLog) {
     let _readerIndex = 0;
-    const header = redoLog.slice(_readerIndex, _readerIndex + 2048);
-    parseRedoLogHeader(header);
+    let header = redoLog.slice(_readerIndex, _readerIndex + 2048);
     _readerIndex += 2048; // 2kb
+    header = parseRedoLogHeader(header);
+    console.log(header);
 
     let block;
     while (_readerIndex < redoLog.length) {
         block = redoLog.slice(_readerIndex, _readerIndex + 512);
         _readerIndex += 512;
-        parseRedoLogBlock(block);
+        block = parseRedoLogBlock(block);
+        console.log(block);
     }
 }
 
-const fs = require('fs');
-const redoLog = fs.readFileSync('./ib_logfile0');
+const redoLog = require('fs').readFileSync('./ib_logfile0');
 parseRedoLog(redoLog);
 // console.log(redoLog.length)
